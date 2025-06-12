83f4d84feb7ff6723a7d4457be717face0aa82c0
Bablu <bablukumar7633017940@gmail.com;>
2025-06-12 10:10:34 +0530

    second commit

---
+++ SprintBootFirstApp/src/main/java/com/SprintBootFirstApp/App.java
@@ -0,0 +1,13 @@
+package com.SprintBootFirstApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
